7798db58f840fea125b946e8bdb934e95a3e7c66
meghnatolani <m.tolani30@gmail.com>
2015-07-23 02:53:40 +0400

    Comments added

--- src/AlienWritterType.java
+++ src/AlienWritterType.java
@@ -6,13 +6,14 @@ public class AlienWritterType {
 	public AlienWritter getAlienWritter(String format) throws FileNotFoundException
 	{
 		int flag=0;
-		
+		/* returns text format */
 		if(format.equals("text"))
 			return new TextWritter();
-		
+		/* return pdf format */
 		else if(format.equals("pdf"))
 			return new PdfWritter();
 		
+		/*for new plugins added */
 		else{	
 			File file2 = new File("Format.txt");
 			if (!file2.exists()) {
@@ -20,13 +21,15 @@ public class AlienWritterType {
 									+ " mention the format in the file and then continue.");
 				return null;	
 				}
-
+			
+			/*Checks if the format asked is present in text file, if so, flag is set */
 			Scanner scan=new Scanner(file2);
 			while(scan.hasNext()){
 				if(format.equalsIgnoreCase(scan.next().trim()))
 						flag=1;	
 				}
 			
+			/* Format class is instantiated if it exists, else error */
 			if(flag==1){
 				try{
 					Class newFormat=Class.forName(format+"Writter");
